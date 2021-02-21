f04930bfdde1f0242ad5975a93194184a926442c
armando <armando.martinez@esausi.com.mx>
2021-02-21 08:25:07 -0600

    Fix origin resource problemas & waiting time to service

--- terraform-modules/monolith-playground/main.tf
+++ terraform-modules/monolith-playground/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "playground" {
   instance_type = "t2.micro"
   user_data = <<EOF
 #!/bin/bash
-yum -y install httpd mariadb.x86_64 mariadb-server java
+sudo yum -y install httpd mariadb.x86_64 mariadb-server java
 echo "<VirtualHost *>" > /etc/httpd/conf.d/tomcat-proxy.conf
 echo "        ProxyPass               /visits      http://localhost:8080/visits" >> /etc/httpd/conf.d/tomcat-proxy.conf
 echo "        ProxyPassReverse       /visits      http://localhost:8080/visits" >> /etc/httpd/conf.d/tomcat-proxy.conf
@@ -17,9 +17,9 @@ mysql -u root -e "CREATE TABLE visits (id bigint(20) NOT NULL AUTO_INCREMENT, co
 mysql -u root -e "INSERT INTO demodb.visits (count) values (0) ;"
 mysql -u root -e "CREATE USER 'monty'@'localhost' IDENTIFIED BY 'some_pass';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'monty'@'localhost' WITH GRANT OPTION;"
-runuser -l ec2-user -c 'cd /home/ec2-user ; curl -O https://raw.githubusercontent.com/giuseppeborgese/effective_devops_with_aws__second_edition/master/terraform-modules/monolith-playground/demo-0.0.1-SNAPSHOT.jar'
-runuser -l ec2-user -c 'cd /home/ec2-user ; curl -O https://raw.githubusercontent.com/giuseppeborgese/effective_devops_with_aws__second_edition/master/terraform-modules/monolith-playground/tomcat.sh'
-cd /etc/systemd/system/ ; curl -O https://raw.githubusercontent.com/giuseppeborgese/effective_devops_with_aws__second_edition/master/terraform-modules/monolith-playground/tomcat.service
+runuser -l ec2-user -c 'cd /home/ec2-user ; curl -O https://raw.githubusercontent.com/esausi/effective_devops_with_aws__second_edition/master/terraform-modules/monolith-playground/demo-0.0.1-SNAPSHOT.jar'
+runuser -l ec2-user -c 'cd /home/ec2-user ; curl -O https://raw.githubusercontent.com/esausi/effective_devops_with_aws__second_edition/master/terraform-modules/monolith-playground/tomcat.sh'
+cd /etc/systemd/system/ ; curl -O https://raw.githubusercontent.com/esausi/effective_devops_with_aws__second_edition/master/terraform-modules/monolith-playground/tomcat.service
 chmod +x /home/ec2-user/tomcat.sh
 systemctl enable tomcat.service
 systemctl start tomcat.service
