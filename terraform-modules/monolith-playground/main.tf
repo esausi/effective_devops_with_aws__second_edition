1ccfa662b4a65ff2555179cadcb8a89f7d7ceccd
armando <armando.martinez@esausi.com.mx>
2021-02-21 16:43:00 -0600

    Specific Java 1.7 version installation

--- terraform-modules/monolith-playground/main.tf
+++ terraform-modules/monolith-playground/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "playground" {
   instance_type = "t2.micro"
   user_data = <<EOF
 #!/bin/bash
-sudo yum -y install httpd mariadb.x86_64 mariadb-server java
+sudo yum -y install httpd mariadb.x86_64 mariadb-server java-1.7.0-openjdk-devel.x86_64
 echo "<VirtualHost *>" > /etc/httpd/conf.d/tomcat-proxy.conf
 echo "        ProxyPass               /visits      http://localhost:8080/visits" >> /etc/httpd/conf.d/tomcat-proxy.conf
 echo "        ProxyPassReverse       /visits      http://localhost:8080/visits" >> /etc/httpd/conf.d/tomcat-proxy.conf
