e4b6823e3780d984b99d471dd3d09a63ecdcb627
armando <armando.martinez@esausi.com.mx>
2021-02-23 07:24:45 -0600

    Update starting java command

--- terraform-modules/monolith-playground/tomcat.sh
+++ terraform-modules/monolith-playground/tomcat.sh
@@ -2,7 +2,7 @@
 case $1 in
  start)
  # start script
- runuser -l ec2-user -c 'export db_pass=some_pass; export db_user=monty; export db_url=jdbc:mysql://localhost:3306/demodb?createDatabaseIfNotExist=true; echo date >> demo-0.0.1.log ;java -jar /home/ec2-user/demo-0.0.1-SNAPSHOT.jar >> demo-0.0.1.log &'
+ runuser -l ec2-user -c 'export db_pass=some_pass; export db_user=monty; export db_url=jdbc:mysql://localhost:3306/demodb?createDatabaseIfNotExist=true; echo `date`>>/home/ec2-user/demo-0.0.1.log; java -jar /home/ec2-user/demo-0.0.1-SNAPSHOT.jar>>/home/ec2-user/demo-0.0.1.log &'
  ;;
  stop)
  # stop script
